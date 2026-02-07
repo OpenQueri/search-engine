1eb2973210bd466701de07f1476906186e406700
Kiril <csgocvadimkiril80@gmail.com>
2026-02-08 00:14:56 +0100

    Add parse web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use scraper::{Html, Selector};
+use regex::Regex;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let url = "https://www.nasa.gov";
+    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+
+    let document = Html::parse_document(&body);
+    let selector = Selector::parse("h1, h2, h3, p").unwrap();
+    
+    let mut text_vec = Vec::new();
+
+    for element in document.select(&selector){
+        let text = element.text().collect::<String>();
+        text_vec.push(text);
+    }
+    println!("input {:?}", text_vec);
+    let twxt = extract_words(text_vec).await;
+    println!("HeshSet {:?}", twxt);
+
+}
+
+async fn extract_words(text: Vec<String>) -> HashMap<String, usize>
+{
+    let re = Regex::new(r"\p{L}+").unwrap();
+    let mut words: HashMap<String, usize> = HashMap::new();
+
+
+    for line in text.iter() {
+        for mat in re.find_iter(line) {
+            let word = mat.as_str().to_lowercase();
+            *words.entry(word).or_insert(0) += 1;
+        }
+    }
+    
+    words
+
 }
--- target/debug/deps/search_engine-4f07a68dd92bdf16.d
+++
@@ -1,5 +0,0 @@
-/home/kiril/OpenQueri/search-engine/target/debug/deps/search_engine-4f07a68dd92bdf16.d: src/main.rs
-
-/home/kiril/OpenQueri/search-engine/target/debug/deps/libsearch_engine-4f07a68dd92bdf16.rmeta: src/main.rs
-
-src/main.rs:
