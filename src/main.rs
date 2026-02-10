095e9476fcffaf2c408b93d913a51e321535889e
Kiril <csgocvadimkiril80@gmail.com>
2026-02-10 20:03:13 +0100

    parse work

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,73 @@
-use reqwest;
 use scraper::{Html, Selector};
 use regex::Regex;
 use std::collections::HashMap;
+use std::error::Error;
+use std::sync::{Arc, Mutex};
+use once_cell::sync::Lazy;
+
+#[derive(Debug)]
+struct SiteWords{
+    url: String,
+    words: Vec<(String, usize)>
+}
+
+static WORD_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"\p{L}+").expect("Invalid regex pattern")
+});
 
 #[tokio::main]
-async fn main() {
-    let url = "https://www.nasa.gov";
-    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let link = vec!["https://www.nasa.gov"];
+
+    let tasks: Vec<_> = link.iter().map(|&url| parse(url)).collect();
+    let results = futures::future::try_join_all(tasks).await?;
+
+
+    for site in results {
+        println!("{:?}", site);
+        println!("✅ {}: {} слов", site.url, site.words.len());
+    }
+    
+    Ok(())
+}
+
+async fn parse(link: &str) -> Result<SiteWords, Box<dyn Error>>{
+
+    let body = reqwest::get(link).await?.text().await?;
 
     let document = Html::parse_document(&body);
-    let selector = Selector::parse("h1, h2, h3, p").unwrap();
+    let selector = Selector::parse("h1, h2, h3, p")?;
     
-    let mut text_vec = Vec::new();
+    let mut text_fragments = Vec::new();
 
     for element in document.select(&selector){
-        let text = element.text().collect::<String>();
-        text_vec.push(text);
+        for text_piece in element.text(){
+            text_fragments.push(text_piece);
+        }
     }
-    println!("input {:?}", text_vec);
-    let twxt = extract_words(text_vec).await;
-    println!("HeshSet {:?}", twxt);
+
+    let words = extract_words(&text_fragments).await?;
+
+    Ok(SiteWords{
+        url: link.to_string(),
+        words: words,
+    })
 
 }
 
-async fn extract_words(text: Vec<String>) -> HashMap<String, usize>
+async fn extract_words(text_fragments: &[&str]) -> Result<Vec<(String, usize)>, Box<dyn Error>>
 {
-    let re = Regex::new(r"\p{L}+").unwrap();
     let mut words: HashMap<String, usize> = HashMap::new();
 
-
-    for line in text.iter() {
-        for mat in re.find_iter(line) {
-            let word = mat.as_str().to_lowercase();
-            *words.entry(word).or_insert(0) += 1;
-        }
+    for &fragment in text_fragments {
+        WORD_REGEX
+            .find_iter(fragment)
+            .map(|mat: regex::Match<'_>| mat.as_str().to_lowercase())
+            .for_each(|word| {
+                *words.entry(word).or_insert(0) += 1;
+            });
     }
+    let vec: Vec<(String, usize)> = (*words.into_iter().collect::<Vec<(String, usize)>>()).to_vec();
     
-    words
-
+    Ok(vec)
 }
